e2c3cc7c5e19cb252b6d83baad29419b91013074
Darshan Chandan <darshanchandan2508@gmail.com>
2025-10-04 18:12:03 +0530

    Init

---
+++ src/main/java/in/certificatemanager/certWatch/CertWatchApplication.java
@@ -0,0 +1,13 @@
+package in.certificatemanager.certWatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertWatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/certificatemanager/certWatch/CertWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package in.certificatemanager.certWatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
